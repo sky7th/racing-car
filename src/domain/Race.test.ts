d446d22491755a4c073056dafc2aa3bafddeab28
Taehwa Lee <xoghk0321@gmail.com>
2020-06-20 12:21:07 +0900

    feat 경주가 끝날 때까지 경주를 한다

--- src/domain/Race.test.ts
+++ src/domain/Race.test.ts
@@ -8,4 +8,12 @@ describe('Race 클래스', () => {
     const race = new Race(raceSetting);
     expect(race).toBeInstanceOf(Race);
   });
+
+  it('시도 횟수가 0이 되면 경주가 끝난다.', () => {
+    const race = new Race(raceSetting);
+    race.move();
+    race.move();
+    race.move();
+    expect(race.isComplete()).toEqual(true);
+  });
 });
--- src/domain/Race.ts
+++ src/domain/Race.ts
@@ -3,11 +3,22 @@ import RaceSetting from "./RaceSetting";
 
 export default class Race {
 
+  private static readonly ZERO_COUNT: number = 0;
+
   private _cars: Cars;
   private _movingCount: number;
 
   constructor(raceSetting: RaceSetting) {
-    this._cars = raceSetting.cars
+    this._cars = raceSetting.cars;
     this._movingCount = raceSetting.movingCount;
+  }
+
+  isComplete() {
+    return this._movingCount === Race.ZERO_COUNT;
+  }
+
+  move() {
+    this._cars.move();
+    this._movingCount -= 1;
   }
 }
